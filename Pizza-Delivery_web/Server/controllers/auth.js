264726bb480700322b994efa346ccfa3d490e0f1
Edasgh <edas25564@gmail.com>
2024-05-28 17:54:05 +0530

    "initializes the auth controller"

---
+++ Pizza-Delivery_web/Server/controllers/auth.js
@@ -0,0 +1,13 @@
+const async_handler=require("express-async-handler");
+
+const User = require("../models/Usermodel");
+
+//function to register user
+
+//function to login user
+
+//function to view logged in user's details
+
+//function to edit logged in user's details
+
+//function to change password || forgot password  function
