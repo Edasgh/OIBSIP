0961d4073db48d55802bb0f94781f2006b63ba86
Edasgh <edas25564@gmail.com>
2024-05-31 19:36:48 +0530

    "Header part completed"

--- Pizza-Delivery_web/Client/src/pages/Home/Home.jsx
+++ Pizza-Delivery_web/Client/src/pages/Home/Home.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import "./Home.css";
-import Carousel from '../../components/Carousel/Carousel';
+import Header from '../../components/Header/Header';
+
 
 const Home = () => {
   return (
-  <>
-   <div className="homepage">
-        <Carousel/>
-   <h1>Hello this is homepage</h1>
-      </div>
-   </>
+    <>
+    <Header />
+    <h1>Hello this is homepage</h1>
+    </>
   )
 }
 
